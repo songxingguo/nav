d19427b9b779d7bc73d40174dee9644f379c0d5b
songxingguo <1328989942@qq.com>
2018-08-29 11:56:07 +0800

    init nav

---
+++ js/more.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(".subNav").click(function() {
+        $(this).toggleClass("currentDd").siblings(".subNav").removeClass("currentDd")
+        $(this).toggleClass("currentDt").siblings(".subNav").removeClass("currentDt")
+
+
+        $(this).next(".navContent").slideToggle(500).siblings(".navContent").slideUp(500);
+    });
+});
